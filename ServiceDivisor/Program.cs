65467cafb360b607ca68357331d7821442e8ded6
DG15GRANJA\Rafael Granja <rafaelgranja2003@gmail.com>
2023-08-07 12:28:11 -0300

    Estrutura do arquivo de serviço criada e em testes

---
+++ ServiceDivisor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace ServiceDivisor
+{
+    class Program:ServiceBase
+    {
+
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                    new Service1(args)
+            };
+            Run(ServicesToRun);
+        }
+
+    }
+}
